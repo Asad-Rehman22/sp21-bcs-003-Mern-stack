db4cc8c3e8122429d43490f0f9ccca01133752d6
Asad-Rehman22 <126698178+Asad-Rehman22@users.noreply.github.com>
2023-12-21 14:39:17 +0500

    commit

--- term_project/server.js
+++ term_project/server.js
@@ -1,41 +1,45 @@
 // Import the Express module
 const express = require('express');
 const path = require('path');
+var session = require('express-session');
 const mongoose = require('mongoose');
 
 // Create an instance of the Express application
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const mongoURI = 'mongodb://localhost::27017/nhb';
-
-mongoose.connect(mongoURI)
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((err) => {
-    console.error(`MongoDB connection error: ${err.message}`);
-  });
-
-mongoose.connection.on('disconnected', () => {
-  console.log('Disconnected from MongoDB');
-});
-
-process.on('SIGINT', () => {
-  mongoose.connection.close(() => {
-    console.log('MongoDB connection disconnected through app termination');
-    process.exit(0);
-  });
-});
+mongoose
+    .connect('mongodb://localhost:27017/nhb', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.log('Error connecting to MongoDB', err.message);
+    });
 
 app.use(express.static('public'));
 
-// Define a route to serve the HTML page
+app.use(
+    session({
+        secret: 'My Open Secret!',
+        resave: false,
+        saveUninitialized: true,
+    })
+);
+
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'home.html'));
+    res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
+
+app.use('/', require('./routes/authentication'));
 
 // Set the server to listen on port 3000
 const port = 3000;
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(Server is running on http://localhost:${port});
 });
